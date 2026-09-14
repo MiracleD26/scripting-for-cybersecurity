b27f06a2cee26740666a963d119101c580f35935
Miracle <miracled2k12@gmail.com>
2026-09-14 15:10:41 +0000

    Complete Lab 1

---
+++ lab01/student.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+
+studentNum = input("What is your student number?")
+
+year  = input("What is your year of study? ")
+
+print("Hello, " + name )
+
+print("Student Number: " + studentNum )
+
+print("Year of study: " + year )
